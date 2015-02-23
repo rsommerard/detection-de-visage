5e158dc875f71240c595ac615ab97230a6744ea5
Romain SOMMERARD <rsommerard@gmail.com>
2015-02-23 12:16:36 +0100

    Update

--- main.py
+++ main.py
@@ -1,23 +1,33 @@
-import numpy as np
+# coding:latin-1
+import sys, os
 import cv2
-
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
-
-img = cv2.imread('yaleB112.pgm')
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-
-print(faces)
-for (x,y,w,h) in faces:
-  img = cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-  roi_gray = gray[y:y+h, x:x+w]
-  roi_color = img[y:y+h, x:x+w]
-  eyes = eye_cascade.detectMultiScale(roi_gray)
-  for (ex,ey,ew,eh) in eyes:
-    cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
-
-cv2.imshow('img', img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+  
+def detecte_visages(image, image_out, show = False):
+    # on charge l'image en mémoire
+    img = cv2.imread(image)
+    # on charge le modèle de détection des visages
+    face_model = cv2.CascadeClassifier("opencv/data/haarcascades/haarcascade_frontalface_alt2.xml")
+     
+     
+    # détection du ou des visages
+    faces = face_model.detectMultiScale(img)
+     
+    # on place un cadre autour des visages
+    print ("nombre de visages", len(faces), "dimension de l'image", img.shape, "image", image)
+    for face in faces:
+        cv2.rectangle(img, (face[0], face[1]), (face[0] + face[2], face[0] + face[3]), (255, 0, 0), 3)
+         
+    # on sauvegarde le résultat final
+    cv2.imwrite(image_out, img)
+     
+    # pour voir l'image, presser ESC pour sortir
+    if show :
+        cv2.imshow("visage",img)
+        if cv2.waitKey(5000) == 27: cv2.destroyWindow("visage")
+   
+if __name__ == "__main__":
+    # applique 
+    for file in os.listdir(".") :
+        if file.startswith("visage") : continue # déjà traité
+        if os.path.splitext(file)[-1].lower() in [".jpg", ".jpeg", ".png" ] :
+            detecte_visages (file, "visage_" + file)
