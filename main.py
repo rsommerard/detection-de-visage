9b475f061d3e2a62277902b5c11e16ac147d66ab
Romain SOMMERARD <rsommerard@gmail.com>
2015-04-03 19:19:57 +0200

    Update

--- main.py
+++ main.py
@@ -1,60 +1,114 @@
-# coding: utf-8
-import sys, os
+# coding=utf-8
+
 import cv2
-
-def detecte_visages(image, image_out, show = False):
-    # on charge l'image en mémoire
-    img = cv2.imread(image)
-    # on charge le modèle de détection des visages
-    face_model = cv2.CascadeClassifier("opencv/data/haarcascades/haarcascade_frontalface_alt2.xml")
-
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-    # détection du ou des visages
-    faces = face_model.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30), flags = cv2.cv.CV_HAAR_SCALE_IMAGE)
-
-    # on place un cadre autour des visages
-    print ("nombre de visages", len(faces), "dimension de l'image", img.shape, "image", image)
-    for face in faces:
-        cv2.rectangle(img, (face[0], face[1]), (face[0] + face[2], face[0] + face[3]), (255, 0, 0), 3)
-
-    # on sauvegarde le résultat final
-    cv2.imwrite(image_out, img)
-
-    # pour voir l'image, presser ESC pour sortir
-    if show :
-        cv2.imshow("visage",img)
-        if cv2.waitKey(5000) == 27: cv2.destroyWindow("visage")
-
-if __name__ == "__main__":
-    # applique
-    for file in os.listdir(".") :
-        if file.startswith("visage") : continue # déjà traité
-        if os.path.splitext(file)[-1].lower() in [".jpg", ".jpeg", ".png" ] :
-            detecte_visages (file, "visage_" + file, True)
-
-
-"""import cv2
 import sys
-# Get user supplied values
-imagePath = sys.argv[1]
-cascPath = sys.argv[2]
-# Create the haar cascade
-faceCascade = cv2.CascadeClassifier(cascPath)
-# Read the image
-image = cv2.imread(imagePath)
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-# Detect faces in the image
-faces = faceCascade.detectMultiScale(
-gray,
-scaleFactor=1.1,
-minNeighbors=5,
-minSize=(30, 30),
-flags = cv2.cv.CV_HAAR_SCALE_IMAGE
-)
-print "Found {0} faces!".format(len(faces))
-# Draw a rectangle around the faces
-for (x, y, w, h) in faces:
-  cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
-  cv2.imshow("Faces found", image)
-  cv2.waitKey(0)"""
+import os
+import random
+import math
+import numpy
+
+###########################
+# Création des eigenfaces #
+###########################
+
+CASC_PATH = 'haarcascade_frontalface_default.xml'
+IMAGES_PATH = 'yalefaces/'
+EIGENFACES_PATH = 'yaleeigenfaces/'
+
+faceCascade = cv2.CascadeClassifier(CASC_PATH)
+
+for dirname in os.listdir(IMAGES_PATH):
+    if dirname == '.DS_Store':
+        continue
+
+    i = 0
+    for filename in os.listdir(IMAGES_PATH + dirname):
+        if filename == '.DS_Store':
+            continue
+
+        image = cv2.imread(IMAGES_PATH + dirname + '/' + filename)
+        print(IMAGES_PATH + dirname + '/' + filename)
+
+        faces = faceCascade.detectMultiScale(image, scaleFactor=1.2, minNeighbors=5,
+            minSize=(30, 30), flags = cv2.cv.CV_HAAR_SCALE_IMAGE)
+
+        print "Found {0} faces!".format(len(faces))
+
+        if not os.path.exists(EIGENFACES_PATH + dirname):
+            os.makedirs(EIGENFACES_PATH + dirname)
+
+
+        for (x, y, w, h) in faces:
+            resized_image = cv2.resize(image[y:(y+h), x:(x+w)], (100, 100))
+            cv2.imwrite(EIGENFACES_PATH + dirname + '/' + str(i) + '.jpeg', resized_image)
+            i += 1
+
+        for (x, y, w, h) in faces:
+            cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+print('-' * 80)
+
+######################################
+# Création du csv pour le Classifier #
+######################################
+
+label = 0
+
+SEPARATOR = ";"
+FACES_FOLDER = "yalefaces/"
+
+for dirname, dirnames, filenames in os.walk(FACES_FOLDER):
+    if dirname == '.DS_Store':
+        continue
+
+    for subdirname in dirnames:
+        if subdirname == '.DS_Store':
+            continue
+
+        subject_path = os.path.join(dirname, subdirname)
+        for filename in os.listdir(subject_path):
+            if filename == '.DS_Store':
+                continue
+            abs_path = "%s/%s" % (subject_path, filename)
+            print "%s%s%s" % (abs_path, SEPARATOR, label)
+
+        label += 1
+
+print('-' * 80)
+
+########################################
+# Apprentissage et prédiction du sujet #
+########################################
+
+CSV_FILE = 'faces.csv'
+
+with open(CSV_FILE, 'r') as file:
+    csv = file.readlines()
+
+ratio = 0.1
+random.shuffle(csv)
+size = int(math.floor(ratio*len(csv)))
+training_data, testing_data = csv[size:], csv[:size]
+
+print('Nb. training data: ' + str(len(training_data)))
+print('Nb. testing data: ' + str(len(testing_data)))
+
+print('-' * 40)
+
+data = {}
+
+for line in training_data:
+    filename, label = line.strip().split(';')
+
+    if int(label) in data:
+        numpy.append(data[int(label)], cv2.imread(filename, cv2.CV_LOAD_IMAGE_GRAYSCALE))
+    else:
+        data[int(label)] = cv2.imread(filename, cv2.CV_LOAD_IMAGE_GRAYSCALE)
+
+model = cv2.createEigenFaceRecognizer()
+model.train(data.values(), numpy.array(data.keys()))
+
+for line in testing_data:
+    filename, label =  line.strip().split(';')
+    predicted_label = model.predict(cv2.imread(filename, cv2.CV_LOAD_IMAGE_GRAYSCALE))
+    print 'Predicted: %(predicted)s  Actual: %(actual)s' %  {"predicted": predicted_label[0], "actual": label}
