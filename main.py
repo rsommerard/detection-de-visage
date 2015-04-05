e8dd37c73bed70d3407276205d3e26f9f3170682
Romain SOMMERARD <rsommerard@gmail.com>
2015-04-05 12:52:35 +0200

    Stop here

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 # coding=utf-8
 
 import cv2
-import sys
 import os
 import random
 import math
@@ -26,13 +25,16 @@ for dirname in os.listdir(IMAGES_PATH):
         if filename == '.DS_Store':
             continue
 
-        image = cv2.imread(IMAGES_PATH + dirname + '/' + filename)
+        image = cv2.imread(IMAGES_PATH + dirname + '/' + filename, cv2.CV_LOAD_IMAGE_GRAYSCALE)
         print(IMAGES_PATH + dirname + '/' + filename)
 
         faces = faceCascade.detectMultiScale(image, scaleFactor=1.2, minNeighbors=5,
             minSize=(30, 30), flags = cv2.cv.CV_HAAR_SCALE_IMAGE)
 
         print "Found {0} faces!".format(len(faces))
+
+        if(len(faces) > 1):
+            continue
 
         if not os.path.exists(EIGENFACES_PATH + dirname):
             os.makedirs(EIGENFACES_PATH + dirname)
@@ -54,16 +56,14 @@ print('-' * 80)
 
 CSV_FILE = 'faces.csv'
 SEPARATOR = ";"
-FACES_FOLDER = "faces/"
+FACES_FOLDER = "eigenfaces/"
 
-label = ''
+label = 0
 content = ''
 
 for dirname, dirnames, filenames in os.walk(FACES_FOLDER):
     if dirname == '.DS_Store':
         continue
-
-    print('>>>' + dirname)
 
     for subdirname in dirnames:
         if subdirname == '.DS_Store':
@@ -75,6 +75,8 @@ for dirname, dirnames, filenames in os.walk(FACES_FOLDER):
                 continue
             abs_path = "%s/%s" % (subject_path, filename)
             content += "%s%s%s\n" % (abs_path, SEPARATOR, label)
+
+        label += 1
 
 with open(CSV_FILE, 'w') as file:
     file.write(content)
@@ -101,12 +103,13 @@ data = {}
 for line in training_data:
     filename, label = line.strip().split(';')
 
-    if int(label) in data:
+    if label in data:
         numpy.append(data[int(label)], cv2.imread(filename, cv2.CV_LOAD_IMAGE_GRAYSCALE))
     else:
         data[int(label)] = cv2.imread(filename, cv2.CV_LOAD_IMAGE_GRAYSCALE)
 
 model = cv2.createEigenFaceRecognizer()
+print(data.keys())
 model.train(data.values(), numpy.array(data.keys()))
 
 for line in testing_data:
